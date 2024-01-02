a71c7d9361e90e5a2e9f7a4450be80ab664cb015
jlindsteadt <joshlindsteadt@gmail.com>
2024-01-01 19:52:59 -0700

    update app.R

--- app.R
+++ app.R
@@ -4,10 +4,4 @@
 
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
-
-# profvis(
-# print(
-    CoT::run_app() # add parameters here (if any)
-# )
-# )
-
+CoT::run_app() # add parameters here (if any)
